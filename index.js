a05e3af7bc92bdae3b9d83931e680334ddbbed09
DavitBoo <davidmarras@gmail.com>
2024-10-27 13:35:04 +0100

    session management

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const session = require('express-session');
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
 const { PrismaClient } = require("@prisma/client");
+const PrismaSessionStore = require('prisma-session-store')(session);
 
 const app = express();
 const prisma = new PrismaClient();
@@ -10,9 +12,31 @@ const prisma = new PrismaClient();
 app.use(express.json()); // Sin este middleware, req.body sería undefined
 app.use(express.urlencoded({ extended: false })); // middleware esencial para procesar datos enviados desde formularios HTML tradicionales
 
+
+
+// Set up express-session with PrismaSessionStore
+app.use(
+  session({
+    secret: 'mi_clave',
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000, 
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000, // Session expiry time in milliseconds (1 day)
+    },
+  }),
+)
+
+
 // Initialize Passport
 app.use(passport.initialize());
+app.use(passport.session());  
 
+// Passport Local Strategy
 passport.use(
   new LocalStrategy({ usernameField: "email" }, async (email, password, done) => {
     try {
@@ -28,6 +52,22 @@ passport.use(
     }
   })
 );
+
+// Se serializa para seleccionar solo el ID del usuario para guardarlo en la sesión
+passport.serializeUser((user, done) => {
+  done(null, user.id);    
+});
+
+// Al tener el id guardado en la sesión buscamos los datos del usuario en la bbdd con ese id
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.user.findUnique({ where: { id } });
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
+
 
 // routes
 app.post("/register", async (req, res) => {
@@ -46,8 +86,27 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post("/login", passport.authenticate("local", { session: false }), (req, res) => {
+app.post("/login", passport.authenticate("local", { failureRedirect: '/login-fail' }), (req, res) => {
   res.status(200).json({ message: "Logged in successfully" });
+});
+
+app.get('/login-fail', (req, res) => {
+  res.status(401).json({ message: 'Login failed' });
+});
+
+app.get('/logout', (req, res) => {
+  req.logout((err) => {             // logout existe el passport
+    if (err) return res.status(500).json({ message: 'Logout failed' });
+    res.status(200).json({ message: 'Logged out successfully' });
+  });
+});
+
+app.get('/profile', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.status(200).json(req.user);
+  } else {
+    res.status(401).json({ message: 'Not authenticated' });
+  }
 });
 
 // Start the server
